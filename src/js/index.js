a77181a7a10b7c368ed84d6059c4182828ddefc1
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-09-10 10:37:57 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import '../styles/main.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+fetch('../assets/tools.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const toolList = document.getElementById('tool-list');
+    const toolsHtml = _.map(data.tools, tool => `<li>${tool.name}</li>`).join('');
+    toolList.innerHTML = `<ul>${toolsHtml}</ul>`;
+  })
+  .catch(error => {
+    console.error('Error fetching tools:', error);
+  });
+
+});
